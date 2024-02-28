19c3c2ab18d372648fd5af88c2501455a6d28721
brianamaftei <briana.maftei@gmail.com>
2024-02-28 15:59:36 +0200

    si exercitiul 2 gata, 3 in progres

--- Tema 1/exercitiul1.py
+++ Tema 1/exercitiul1.py
@@ -2,13 +2,17 @@ def find_u():
     m = 0
     u = 0
     while True:
-        if 10 ** (-m) <= 0:
-            print("Valoarea lui u:", '{0:.1000f}'.format(u))
+        if 10 ** (-m) == 0:
+            print("Valoarea lui u cu zecimale:", '{0:.1000f}'.format(u))
+            print("Valoarea lui u:", u)
             print("Valoarea lui u urmator:", 10**(-m)+1)
             print("Valaorea lui m:", m)
-            break
+            return u
         else:
             u = 10 ** (-m)
         m += 1
 
 find_u()
+
+
+
---
+++ Tema 1/exercitiul2.py
@@ -0,0 +1,24 @@
+from exercitiul1 import find_u
+from decimal import Decimal, getcontext
+
+
+def verify_operation_plus():
+    getcontext().prec = 500
+    x = Decimal('1.0')
+    u = Decimal(find_u())
+    y = u / Decimal('10')
+    z = u / Decimal('10')
+    result1 = (x + y) + z
+    result2 = x + (y + z)
+    print("Rezultatul 1:", result1)
+    print("Rezultatul 2:", result2)
+    return result1 != result2
+
+
+print(verify_operation_plus())
+
+
+def verify_operation_multiply(x, y, z):
+    return (x * y) * z != x * (y * z)
+
+print(verify_operation_multiply(0.4, 0.2, 0.3))
