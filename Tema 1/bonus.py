8912a7f3a90504fdb28ce0ec1dcd53dee1ea30f4
brianamaftei <briana.maftei@gmail.com>
2024-03-03 20:03:30 +0200

    Modification homework 1

--- Tema 1/bonus.py
+++ Tema 1/bonus.py
@@ -4,11 +4,13 @@ from exercitiul3 import list_of_functions
 
 
 def sin_S(T_x, a):
-    return T_x(a) / math.sqrt(1 + (T_x(a) ** 2))
+    # return T_x(a) / math.sqrt(1 + (T_x(a) ** 2))
+    return (1 - T_x((2*a-math.pi)/4)**2) / (1 + T_x((2*a-math.pi)/4)**2)
 
 
 def cos_S(T_x, a):
-    return 1 / math.sqrt(1 + (T_x(a) ** 2))
+    # return 1 / math.sqrt(1 + (T_x(a) ** 2))
+    return (1 - (T_x(a/2))**2) / (1 + (T_x(a/2))**2)
 
 
 def approximations(f, a):
@@ -17,6 +19,6 @@ def approximations(f, a):
 
 
 print("Sinus:")
-approximations(sin_S, 30)
+approximations(sin_S, math.pi/4)
 print("Cosinus:")
-approximations(cos_S, 30)
+approximations(cos_S, math.pi/4)
