148df97217745ea5fc28a39a271088650dfcbfae
Dumitriu Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2024-03-09 20:25:43 +0200

    ex 3, inca nu e gata

--- Tema 3/exercitiul.py
+++ Tema 3/exercitiul.py
@@ -2,7 +2,7 @@ import random
 import math
 import numpy as np
 
-epsilon = 10 ** (-5)
+epsilon = 1e-5
 
 
 def calculate_vector(A, n, s):
@@ -88,24 +88,48 @@ def solve_system(R, n, b):
     return x
 
 
+def solve_system2(R, b):
+    return np.linalg.solve(R, b)
+
+
 def find_x_qr_with_lib(A, n, b):
     Q, R = np.linalg.qr(A)
-    x = solve_system(R, n, b)
+    x = solve_system(R, n, np.dot(Q.T, b))
     return x
 
+
+def calculate_second_norm(A_init, X_house, b_init):
+    print("b_init:", b_init)
+    print("A_init:", A_init)
+    print("X_house:", X_house)
+    A_init_x_house = np.dot(A_init, X_house)
+    print("A_init_x_house:", A_init_x_house)
+
+    norm = np.linalg.norm(A_init_x_house - b_init)
+    print("Norm between A_init*X_house and b_init:", norm)
+    print("------------------")
+
+
+def qr_decomposition(A):
+    Q, R = np.linalg.qr(A)
+    return Q, R
 
 
 def main():
     n = 3
-    s = generate_vector_s(n)
-    A = generate_matrix(n)
+    s = [3, 2, 1]
+    A = [[0, 0, 4], [1, 2, 3], [0, 1, 2]]
+   # s = generate_vector_s(n)
+   # A = generate_matrix(n)
     A_init = np.copy(A)
+    A_init = np.array(A_init, dtype=np.float32)
     print("A:")
     print_matrix(A)
     print()
     print("s:", s)
     print()
     b = calculate_vector(A, n, s)
+    b_init = np.copy(b)
     print("b:", b)
     print("------------------")
     print()
@@ -118,14 +142,31 @@ def main():
     print_matrix(R)
     print("------------------")
     print()
+    # Q, R = qr_decomposition(A_init)
+    print("Q:")
+    print_matrix(Q)
+    print("R:")
+    print_matrix(R)
+    print("------------------")
 
-    X_house = solve_system(R, n, b)
-    x_QR = find_x_qr_with_lib(A_init, n, b)
+    X_house = solve_system(R, n, np.dot(Q.T, b_init))
+
+    x_QR = find_x_qr_with_lib(A_init, n, b_init)
 
     print("norma:", calculate_norm(x_QR, X_house))
     print("X_house:", X_house)
+    print("s: ", s)
     print("x_QR:", x_QR)
     print()
+    print("------------------")
+    print()
+    print("X_house:", X_house)
+
+    calculate_second_norm(A_init, X_house, b_init)
+
+    print("x_QR:", x_QR)
+    print("A_init:", A_init)
+
 
 if __name__ == "__main__":
     main()
