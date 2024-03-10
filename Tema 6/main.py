7461c1da5aca15c90caf688b5def1eb0a9015001
brianamaftei <briana.maftei@gmail.com>
2024-03-10 10:17:10 +0200

    uitasem de module

--- Tema 6/main.py
+++ Tema 6/main.py
@@ -24,17 +24,9 @@ def function_1(x):
     return x ** 4 - 12 * x ** 3 + 30 * x ** 2 + 12
 
 
-def function_2(x):
-    return -23 * x ** 2 + 20 * x + 50
-
-
-def least_squares_method(f, a, b, n, m, x_):
-    h = (b - a) / n
-    x = [a + i * h for i in range(n + 1)]
+def least_squares_method(n, m, x_, x, y):
     print("x= ", x)
     print("h= ", h)
-    # y = [f(x[i]) for i in range(n + 1)]
-    y = [50, 47, -2, -121, -310, -545]
 
     B = np.zeros((m + 1, m + 1))
     for i in range(m + 1):
@@ -59,7 +51,7 @@ def horner_method(c, x_):
     d = c[len(c) - 1]
     for i in range(len(c) - 2, -1, -1):
         d = d * x_ + c[i]
-    print("With Horner P(x)= ", d)
+    return d
 
 
 if __name__ == "__main__":
@@ -71,21 +63,24 @@ if __name__ == "__main__":
     # x_list = [0, 2, 4, 6]
     # fx_list = [1, -3, 49, 253]
     # x=1
+    result = progressive_newton_interpolation(x_list, fx_list, x)
+    print("L_n(x) =", result)
+    print("|L_x- f(x)| =", abs(result - function_1(x)))
 
-    print("L_n(x) =", progressive_newton_interpolation(x_list, fx_list, x))
-
-    # a = 1
-    # b = 5
-    # n = 4
-    # m = 4
-    # x_ = 3
-    # c = least_squares_method(function_1, a, b, n, m, x_)
-    # horner_method(c, x_)
-
-    a = 0
+    a = 1
     b = 5
-    n = 5
-    m = 2
-    x_ = 1.5
-    c = least_squares_method(function_2, a, b, n, m, x_)
+    n = 4
+    m = 4
+    x_ = 3
+    h = (b - a) / n
+    x = [a + i * h for i in range(n + 1)]
+    y = [function_1(x[i]) for i in range(n + 1)]
+    c = least_squares_method(n, m, x_, x, y)
     horner_method(c, x_)
+
+    print("With Horner P(x)= ", result)
+
+    print("|P(x)- f(x)| =", abs(result - function_1(x_)))
+
+    difference = sum(np.abs(horner_method(c, x_el) - function_1(x_el)) for x_el in x)
+    print("Difference= ", difference)
